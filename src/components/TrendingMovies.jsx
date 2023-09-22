cea6f9771e4ce1048cf5fe21f5b3835b3b791cf9
1evanter <rita.oleksenkoo@gmail.com>
2023-09-23 00:22:01 +0300

    create TrendingMovies component

---
+++ src/components/TrendingMovies.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const TrendingMovies = ({movies}) => {
+    return (
+        <ul>
+            {
+                movies.map(({id, title}) => {
+                    return (
+                        <li key={id}>
+                            <Link to={`movies/:${id}`}>{title}</Link>
+                        </li>
+                    )
+                })
+            }
+    </ul>
+)
+}
+
+export default TrendingMovies;
