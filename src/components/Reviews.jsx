a549e18e2a7bd8caccd1cda56ac3d674f5234565
1evanter <rita.oleksenkoo@gmail.com>
2023-09-23 16:08:46 +0300

    create Reviews component

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { fetchMovieReviews } from "api";
+
 const Reviews = () => {
+    const { movieId } = useParams();
+    const [reviews, setReviews] = useState([])
+
+useEffect(() => {
+
+    async function getMovieReviews() {
+    try {
+        const movieReviews = await fetchMovieReviews(movieId);
+        if (movieReviews.length) {
+            setReviews(movieReviews);
+    }
+    } catch (error) {
+        console.log(error)
+        }
+    }
+    
+    getMovieReviews();
+}, [movieId])
+
     return (
-        <div>Reviews</div>
+        reviews.length > 0 ? (
+            <div>
+                <ul> 
+                    <h4>Reviews</h4>
+                    {reviews.map(({ id, author, content }) => (
+                        <li key={id}>
+                            <p>Author: {author}</p>
+                            <p>{content}</p>
+                        </li>
+                    ))}
+                </ul>
+          </div>  
+       ) : (<div>We don't have any reviews for this movie...</div>)
     )
 }
 
