3d4052064e8477f8284792d037de18d0f058f943
1evanter <rita.oleksenkoo@gmail.com>
2023-09-23 14:41:18 +0300

    Update api.js
    add fetch movieCast, Reviews & Search

--- src/api.js
+++ src/api.js
@@ -13,4 +13,19 @@ const fetchMovieById = async movieId => {
   return response.data;
 };
 
-export {fetchMovies, fetchMovieById}
+const fetchMovieCast = async movieId => {
+  const response = await axios.get(`/3/movie/${movieId}/credits?api_key=${API_KEY}`);
+  return response.data;
+}
+
+const fetchMovieReviews = async movieId => {
+  const response = await axios.get(`/3/movie/${movieId}/reviews?api_key=${API_KEY}`);
+  return response.data;
+}
+
+const fetchMovieSearch = async query => {
+    const response = await axios.get(`/3/search/movie?query=${query}&api_key=${API_KEY}`)
+    return response.data;
+};
+
+export {fetchMovies, fetchMovieById, fetchMovieCast, fetchMovieReviews, fetchMovieSearch}
