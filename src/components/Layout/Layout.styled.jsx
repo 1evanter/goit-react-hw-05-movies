54cfb4fc830045f70fff561d5ee5764f1d240a76
1evanter <rita.oleksenkoo@gmail.com>
2023-09-23 21:41:29 +0300

    Update Layout.styled.jsx

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -2,13 +2,15 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-  max-width: 1200px;
+  max-width: 1000px;
   margin: 0 auto;
   padding: 0px 20px;
+  padding-bottom: 30px;
 `;
 
 const Header = styled.header`
   padding: 16px 0;
+  margin-bottom: 30px;
   border-bottom: 1px solid black;
   `
 
