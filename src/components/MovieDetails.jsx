c9761c01648af4f34d51056b65938c19344fc36f
1evanter <rita.oleksenkoo@gmail.com>
2023-09-23 17:13:33 +0300

    Update MovieDetails.jsx

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,20 +1,20 @@
 const MovieDetails = ({ movieDetails }) => {
-    const { title, overview, poster_path, vote_average } = movieDetails;
+    const { title, overview, poster_path, vote_average, genres } = movieDetails;
 
     return (
         <div>
-            Movie Details
+            
             <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} />
             <h2>{title}</h2>
-            <p>{ vote_average}</p>
+            <p>{vote_average}</p>
+            <h3>Overview</h3>
             <p>{overview}</p>
-            {/* <ul>{genres.map(({ id, name }) => {
-                return (
-                    <li key={id}>{name}</li>
-                )
-            })}</ul> */}
+            <ul>
+                <h3>Genres: </h3>
+                {genres.map(el => 
+                    <li key={el.id}> {el.name}</li>
+            )}</ul>
        </div>
-        
     )
 }
 export default MovieDetails
