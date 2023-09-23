a8a36cb51e16314fa6459c9bc1d4a36ea65091d9
1evanter <rita.oleksenkoo@gmail.com>
2023-09-24 01:19:19 +0300

    Update MovieDetails.styled.jsx

--- src/components/MovieDetails/MovieDetails.styled.jsx
+++ src/components/MovieDetails/MovieDetails.styled.jsx
@@ -14,8 +14,8 @@ color: #DDE6ED;
 background-color: #526D82;
 border: 2px solid #526D82;
 border-radius: 4px;
-transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
 &:hover,
 &:focus {
