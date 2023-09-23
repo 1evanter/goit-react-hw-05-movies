08c2541e6991c461d41b111a6dada5ff82d2c870
1evanter <rita.oleksenkoo@gmail.com>
2023-09-24 01:49:28 +0300

    Update Movies.jsx

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-import { List, Item, Image, StyledLink, Text } from './Movies.styled'
+import { List, Item, Image, Text, StyledLink } from './Movies.styled'
 
 const defaultImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
 
@@ -12,7 +12,7 @@ const Movies = ({movies}) => {
                 {movies && movies.map(({id, title, poster_path}) => {
                     return (
                         <Item key={id}>
-                            <StyledLink to={`movies/${id}`} state={{ from: location }}>
+                            <StyledLink to={`${id}`} state={{ from: location }}>
                                <Image src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : defaultImage} alt={title} />
                                 <Text>{title}</Text>
                                 </StyledLink>
